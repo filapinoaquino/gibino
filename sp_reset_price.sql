b9f4f02914d7f0d950ebd37c608ec0cc1d4232db
David Aquino <filapinoaquino@gmail.com>
2014-11-26 19:57:26 -0500

    made some changes

--- sp_reset_price.sql
+++ sp_reset_price.sql
@@ -1,13 +1,11 @@
---use david
---begin stored procedure
 --exec sp_reset_price
 IF OBJECTPROPERTY(object_id('dbo.sp_reset_price'), N'IsProcedure') = 1
 DROP PROCEDURE [dbo].[sp_reset_price]
 GO
 CREATE PROCEDURE dbo.sp_reset_price
 AS 
-BEGIN TRANSACTION;
-BEGIN TRY
+BEGIN 
+BEGIN TRANSACTION
 
 UPDATE pri
 SET pri.pro_price = pro.pro_base
@@ -15,21 +13,13 @@ FROM t_price pri INNER JOIN t_product pro
 on pri.pro_id = pro.pro_id;
 
 SELECT * FROM T_PRODUCT;
+if @@error <> 0
 
-END TRY
-BEGIN CATCH
-    SELECT 
-        ERROR_NUMBER() AS ErrorNumber
-        ,ERROR_SEVERITY() AS ErrorSeverity
-        ,ERROR_STATE() AS ErrorState
-        ,ERROR_PROCEDURE() AS ErrorProcedure
-        ,ERROR_LINE() AS ErrorLine
-        ,ERROR_MESSAGE() AS ErrorMessage;
+	begin
+		rollback transaction
+		select ' Sale was not completed'
+		return
+	end
 
-    IF @@TRANCOUNT > 0
-        ROLLBACK TRANSACTION;
-END CATCH;
-
-IF @@TRANCOUNT > 0
-    COMMIT TRANSACTION;
-GO
+commit transaction
+END
